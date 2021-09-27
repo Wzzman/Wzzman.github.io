c0a32e95524a9e24b35cf480cd17e72aa4249f85
AO_Home <artonishchenko@gmail.com>
2021-09-27 08:49:34 +0200

    HomeWork, habe alles vorbereitet. Playermenu ist fertig.

--- 4gewinnt.js
+++ 4gewinnt.js
@@ -1,14 +1,27 @@
 // wssp, hier sind alle meine Funktionen gesammelt.
 
+
+let Player1 = {     // объект
+  name: "noname",  // под ключом "name" хранится значение "John"
+  key: 1        // под ключом "age" хранится значение 30
+};
+
+let Player2 = {     // объект
+  name: "noname",  // под ключом "name" хранится значение "John"
+  key: 2        // под ключом "age" хранится значение 30
+};
+
 function alertKomm() {
   alert("Willkommen bei meinem 4Gewinnt-Spiel");
 }
 
 function SaveNames() // save the names
 {
-	let Player1 = document.getElementById("name1").value;
-	let Player2 = document.getElementById("name2").value;
-	ChangeNames(Player1, Player2);
+	 Player1.name = document.getElementById("name1").value;
+	 Player2.name = document.getElementById("name2").value;
+   document.getElementsByClassName('playground')[0].style.display = "initial";
+  
+	ChangeNames(Player1.name, Player2.name);
 	MenuOffner();
 }
 
@@ -20,16 +33,17 @@ function ChangeNames(NM1, NM2) // ändert Playersnamen
 
 function MenuOffner() // schaltet Namesbereich aus
 {
-	document.getElementsByClassName('PlayersMenu')[0].style.display = "none";
+	document.getElementsByClassName('pn')[0].style.display = "none";
 }
 
 function RefreshAll() // schaltet Namesbereich aus
 {
-	document.getElementsByClassName('PlayersMenu')[0].style.display = "initial";
+	document.getElementsByClassName('pn')[0].style.display = "";
 	document.getElementById("name1").value = "";
 	document.getElementById("name2").value = "";
 }
 
 // document.getElementById("demo").innerHTML = "Hello JavaScript"; // geht
-// paam yopte amigos
+
+// childNods
 
